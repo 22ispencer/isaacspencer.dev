6409d38ec5e01dfe5e83511a7243045278d02aa5
Isaac Spencer <22ispencer@gmail.com>
2024-10-10 10:37:35 -0500

    solid

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,11 @@ import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 
+import solidJs from "@astrojs/solid-js";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://isaacspencer.dev",
-  integrations: [tailwind()],
+  integrations: [tailwind(), solidJs()],
   output: "static",
 });
