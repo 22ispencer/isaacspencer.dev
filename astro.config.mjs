6bf1e149632f56098b733eb9197b733f7faf40a6
Isaac Spencer <22ispencer@gmail.com>
2024-10-17 13:23:47 -0500

    react

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,11 @@ import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://isaacspencer.dev",
-  integrations: [tailwind()],
+  integrations: [tailwind(), react()],
   output: "static",
 });
